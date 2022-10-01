3322368d9e8d6e996d20164080b65af6613501ac
michelle <113373744+mfarrell23@users.noreply.github.com>
2022-10-01 16:49:03 -0600

    made the html and sstyle

---
+++ Assets/CSS/script.js
@@ -0,0 +1,7 @@
+var answerA=""document.queryselector("");
+var answerB=""document.quesryselector("");
+var answerC=""document.queryselector("");
+
+
+answerEl.addEventListener("click", showResponse);
+confirm //the correct answer to pop up
