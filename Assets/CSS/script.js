1964039f7d43a1cd162a64f1492a1eac1d4eb35e
michelle <113373744+mfarrell23@users.noreply.github.com>
2022-10-04 10:32:33 -0600

    added java and updated README

--- Assets/CSS/script.js
+++ Assets/CSS/script.js
@@ -1,7 +1,56 @@
-var answerA=""document.queryselector("");
-var answerB=""document.quesryselector("");
-var answerC=""document.queryselector("");
+//Selects the questions element
+var questionbox = document.querySelector(".questionCard");   
 
+let header = document.getElementById("header")
+header.textContent = "This is my question"
+let p1 = document.getElementById("p1").innerHTML = "asfaf";
+
+var next = questionbox.querySelector(".next");
+var index = 0;
+var currentQuestion;
+var questions = [
+        {
+        question:"Who lost to the Bulls in the 90's finals?",
+        answerEl: "Jazz",
+        possibleChoices: "Kings", "Jazz", "Knicks"},
+       
+        {
+        question:"Who was the best point guardin the 90's?",
+        answerEl: "Stockton",
+        possibleChoices: "Stockton", "Jordan", "Bibby"},
+       
+        {
+        question:"What does NBA stand for?",
+        answerEl: "National Basketball Assoc.",
+        possibleChoices: "National Basketball Assoc." , "National Buckets Assoc", "National bucketball Assoc."},
+       
+        {
+        question:"who is not a basketball player?",
+        answerEl: "Depp",
+        possibleChoices:"Noah", "Irving", "Depp"},
+       
+        {
+        question:"What month is Allstar weekend?",
+        answerEl: "Feb",
+        possibleChoices:"June", "Sept", "Feb"},
+
+        {
+        question:"Who is the G.O.A.T?",
+        answerEl: "Michael Jordan",
+        possibleChoices:"Lebron James", "Magic Johnson", "Michael Jordan"}
+    ];
+
+
+
+
+    let questionCard= document.querySelector('.questionCard');
+    questionCard.innerText= questions[0].question;
+
+
+
+if (element.matches ("button")===true){
+    var index= element.parentElement.getAttribute("date-index)");
+}
 
 answerEl.addEventListener("click", showResponse);
 confirm //the correct answer to pop up
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # miniQuiz
+This project is my attempt at creating a small quiz on 90's NBA knowledge.
+I have made a button activated quiz that keeps score of how many questions you got right. 
+I tried to make each question rotate through and have provided a next button when you get the answer wrong.
+If you get the answer correct it will move you to the next question.
+I styled my page with my favorite colors and tryied to impliment the things I have learned.
+I tried to tie the inputs and score with the local storage.
